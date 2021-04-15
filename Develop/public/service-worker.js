6a2a4a7dcf1099da38056b89a957a4f0fb758286
Caroline Tobin <ctobin25>
2021-04-14 20:16:46 -0400

    added manifest file

---
+++ Develop/public/service-worker.js
@@ -0,0 +1,11 @@
+const CACHE_NAME = "static-cache-v2";
+const DATA_CACHE_NAME = "data-cache-v1";
+const FILES_TO_CACHE = [
+    "/",
+    "index.html",
+    "index.js",
+    "styles.css",
+    "/icon/icon-192x192.png",
+    "/icon/icon-512x512.png",
+    "/manifest.webmanifest"
+];
