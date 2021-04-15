f6e898c67669ae0ff09790d44240f7cb1641a833
Caroline Tobin <ctobin25>
2021-04-14 20:24:22 -0400

    service worker working

--- Develop/public/service-worker.js
+++ Develop/public/service-worker.js
@@ -5,7 +5,54 @@ const FILES_TO_CACHE = [
     "index.html",
     "index.js",
     "styles.css",
-    "/icon/icon-192x192.png",
-    "/icon/icon-512x512.png",
+    "/icons/icon-192x192.png",
+    "/icons/icon-512x512.png",
     "/manifest.webmanifest"
 ];
+
+self.addEventListener("install", function (evt) {
+  
+    evt.waitUntil(
+      caches.open(DATA_CACHE_NAME).then((cache) => cache.add("/api/images"))
+    );
+      
+
+    evt.waitUntil(
+      caches.open(CACHE_NAME).then((cache) => cache.addAll(FILES_TO_CACHE))
+    );
+  
+    self.skipWaiting();
+  });
+
+  self.addEventListener("fetch", function(evt) {
+    if (evt.request.url.includes("/api/")) {
+      evt.respondWith(
+        caches.open(DATA_CACHE_NAME).then(cache => {
+          return fetch(evt.request)
+            .then(response => {
+              
+              if (response.status === 200) {
+                cache.put(evt.request.url, response.clone());
+              }
+  
+              return response;
+            })
+            .catch(err => {
+              // Network request failed, try to get it from the cache.
+              return cache.match(evt.request);
+            });
+        }).catch(err => console.log(err))
+      );
+  
+      return;
+    }
+  
+    evt.respondWith(
+      caches.open(CACHE_NAME).then(cache => {
+        return cache.match(evt.request).then(response => {
+          return response || fetch(evt.request);
+        });
+      })
+    );
+  });
+  
